528b992fec308b2f38e28d857afee54b2fc14b01
DynamicSaikat <133014203+DynamicSaikat@users.noreply.github.com>
2023-05-09 18:17:53 +0530

    Delete pre.js

--- pre.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener('load', function() {
-    const preloader = document.querySelector('.preloader');
-    preloader.classList.add('hide-preloader');
-    setTimeout(function() {
-        preloader.style.display = 'block';
-    }, 500);
-
-});
