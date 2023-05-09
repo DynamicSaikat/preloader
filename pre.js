24a0c3c4459a31591c7377f5eefe6bfc5c4e7681
DynamicSaikat <133014203+DynamicSaikat@users.noreply.github.com>
2023-05-09 18:14:24 +0530

    Update pre.js

--- pre.js
+++ pre.js
@@ -2,7 +2,7 @@ window.addEventListener('load', function() {
     const preloader = document.querySelector('.preloader');
     preloader.classList.add('hide-preloader');
     setTimeout(function() {
-        preloader.style.display = 'none';
+        preloader.style.display = 'block';
     }, 500);
 
 });
